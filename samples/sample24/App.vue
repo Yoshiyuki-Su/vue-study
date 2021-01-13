da6bb4bb1309d665081e0675f467a5b1a1c4edf2
Sugiyama Yoshiyuki <yo.sugiyama67@gmail.com>
2021-01-14 07:26:06 +0900

    feat: Add two sample programs.

---
+++ samples/sample24/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header></Header>
+    <Body></Body>
+    <Body></Body>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Body from './Body.vue'
+
+export default {
+  components: {
+    Header,
+    Body
+  }
+}
+</script>
